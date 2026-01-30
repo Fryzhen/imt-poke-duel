84b93452e03801cb75ef9519b07d9d865ba61acd
vrignaudl <lucasvrignaud1@gmail.com>
2026-01-30 16:49:39 +0100

    equipe pokemon

--- src/main/java/fr/pokeduel/game/Player.java
+++ src/main/java/fr/pokeduel/game/Player.java
@@ -3,15 +3,31 @@ package fr.pokeduel.game;
 
 import fr.pokeduel.data.entity.Pokemon;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
     public String nom;
     public boolean isHuman;
-    public List<Pokemon> pokemons;
+    public List<Integer> pokemons;
 
     public Player(String nom, boolean isHuman) {
         this.nom = nom;
         this.isHuman = isHuman;
+        this.pokemons = new ArrayList<>();
+    }
+
+    public void addPokemon(int pokemonId) {
+        if ( this.pokemons.size() >= 6 ) {
+            throw new RuntimeException("Un joueur ne peut pas avoir plus de 6 pokémons");
+        }
+        this.pokemons.add(pokemonId);
+    }
+
+    public void removePokemon(int pokemonId) {
+        if ( !this.pokemons.contains(pokemonId) ) {
+            throw new RuntimeException("Le pokémon avec l'id " + pokemonId + " n'appartient pas au joueur " + this.nom);
+        }
+        this.pokemons.remove(pokemonId);
     }
 }
